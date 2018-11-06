15a2b37e381538378dc384e15b872079da94d1ec
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-11-06 14:23:13 +0100

    Use dotenv-webpack instead of babel env loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   webpack: config => {
+    config.plugins = config.plugins || []
+    config.plugins = [
+      ...config.plugins,
+      // Read the .env file
+      new Dotenv({
+        /* eslint-disable no-undef */
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
+
     if (config.resolve.alias) {
       delete config.resolve.alias.react;
       delete config.resolve.alias["react-dom"];
